e611d4b04d59e3b0e69dfbd929965f392918ceca
EmmaDuane <65099184+EmmaDuane@users.noreply.github.com>
2022-11-09 15:29:47 -0800

    added porterstemmer

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 import json
 import nltk
+from nltk.stem import PorterStemmer
 import pickle
 
-
-### Global variables, 
+### Global variables,
 DATA_PATH = "./data/DEV"
 PRE_TOKEN_DATA_PATH = "./data/tokens_DEV_cache.p"
 PRE_FILENAME_DATA_PATH = "./data/filenames_DEV_cache.p"
-USE_CACHE = True #set to true to load the pre_computed token
+USE_CACHE = False  # set to true to load the pre_computed token
+PS = PorterStemmer()
 ###
 
-def get_all_file_names(data_path = DATA_PATH):
+def get_all_file_names(data_path=DATA_PATH):
     """
     get all the file names from a data_path directory
     return as list
@@ -24,13 +25,14 @@ def get_all_file_names(data_path = DATA_PATH):
 
     print("Generating file names...")
     file_names = []
-    for root,dirs,files in os.walk(DATA_PATH):
+    for root, dirs, files in os.walk(DATA_PATH):
         for file in files:
-            file_names.append(os.path.join(root,file))
+            file_names.append(os.path.join(root, file))
         print(f"Found {len(files)} file under {root} directory")
     print(f"Total number of file found: {len(file_names)}")
-    store_data(file_names,PRE_FILENAME_DATA_PATH)
+    store_data(file_names, PRE_FILENAME_DATA_PATH)
     return file_names
+
 
 def get_token_from_file(file_name):
     """
@@ -40,9 +42,14 @@ def get_token_from_file(file_name):
         data = json.load(f)
         content = data["content"]
         tokens = nltk.tokenize.word_tokenize(content)
-        filtered_tokens = [word.lower() for word in tokens]
+        filtered_tokens = filter(lambda word: word.isalnum(), tokens)
+        filtered_tokens = list(filtered_tokens)
+        for t in range(len(filtered_tokens)):
+            filtered_tokens[t] = filtered_tokens[t].lower()
+            filtered_tokens[t] = PS.stem(filtered_tokens[t])
         return set(filtered_tokens)
-
+    
+    
 def get_all_tokens(dataset):
     """
     get all tokens from the dataset, return as a list
