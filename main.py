0e552b2529a72b411afcbfd784be5f15162a3b4f
Eric <xyy1415238157@icloud.com>
2022-11-09 07:41:25 -0800

    found 421038 tokens, f

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import pickle
 
 
 ### Global variables, 
-DATA_PATH = "./data/ANALYST"
-PRE_TOKEN_DATA_PATH = "./data/tokens_cache.p"
-LOAD_DATA = False #set to true to load the pre_computed token
+DATA_PATH = "./data/DEV"
+PRE_TOKEN_DATA_PATH = "./data/tokens_DEV_cache.p"
+PRE_FILENAME_DATA_PATH = "./data/filenames_DEV_cache.p"
+USE_CACHE = False #set to true to load the pre_computed token
 ###
 
 def get_all_file_names(data_path = DATA_PATH):
@@ -15,13 +16,20 @@ def get_all_file_names(data_path = DATA_PATH):
     get all the file names from a data_path directory
     return as list
     """
+    if USE_CACHE:
+        print("Getting file names from cache...")
+        data = read_data(PRE_FILENAME_DATA_PATH)
+        print(f"Total number of file read: {len(data)}")
+        return data
+
     print("Generating file names...")
     file_names = []
     for root,dirs,files in os.walk(DATA_PATH):
         for file in files:
             file_names.append(os.path.join(root,file))
         print(f"Found {len(files)} file under {root} directory")
-    print(f"Total number of file found {len(file_names)}")
+    print(f"Total number of file found: {len(file_names)}")
+    store_data(file_names,PRE_FILENAME_DATA_PATH)
     return file_names
 
 def get_token_from_file(file_name):
@@ -38,17 +46,23 @@ def get_token_from_file(file_name):
 
 def get_all_tokens(dataset):
     """
-    get all tokens from the dataset, return as a set
+    get all tokens from the dataset, return as a list
     """
+    if USE_CACHE:
+        print("Getting tokens from cache...")
+        return read_data(PRE_TOKEN_DATA_PATH) 
+
     print("generating tokens...")
     tokens = set()
     size = len(dataset)
-    print_every = 100 #print every 20 files
+    print_every = 500 #print every 500 files
     for i,filename in enumerate(dataset):
         tokens = tokens.union(get_token_from_file(filename))
         if (i+1)%print_every == 0:
             print(f"{i+1} files completed, {100*(i+1)/size:.2f}%")
     print(f"token generated completed with size {len(tokens)}")
+    tokens = list(tokens)
+    store_data(tokens,PRE_TOKEN_DATA_PATH)
     return tokens
 
 def store_data(data,filename=PRE_TOKEN_DATA_PATH):
@@ -56,19 +70,13 @@ def store_data(data,filename=PRE_TOKEN_DATA_PATH):
         pickle.dump(data,f)
     print(f"writting data to {filename}")
 
-def read_data(filename=PRE_TOKEN_DATA_PATH):
+def read_data(filename):
     with open(filename,"rb") as f:
         return pickle.load(f)
 
 def main():
-    tokens = []
-    if LOAD_DATA:
-        tokens = read_data(PRE_TOKEN_DATA_PATH)
-    else:
-        dataset = get_all_file_names(DATA_PATH)
-        tokens = list(get_all_tokens(dataset))
-        store_data(tokens,PRE_TOKEN_DATA_PATH)
-    
+    dataset = get_all_file_names(DATA_PATH)
+    tokens = get_all_tokens(dataset)
     
     
     
