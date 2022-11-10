3dceae4e98ba11c20bbb0d2d3844780bf3cdb70e
Eric <xyy1415238157@icloud.com>
2022-11-09 20:55:36 -0800

    check for doctype html, using soup to parse, add max and min count

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import os
 import json
 import nltk
 import pickle
-
+import time
+from bs4 import BeautifulSoup
 
 ### Global variables, 
 DATA_PATH = "./data/DEV"
 PRE_TOKEN_DATA_PATH = "./data/tokens_DEV_cache.p"
-PRE_FILENAME_DATA_PATH = "./data/filenames_DEV_cache.p"
-USE_CACHE = True #set to true to load the pre_computed token
+PRE_FILENAME_DATA_PATH = "./data/filenames_DEV_cache.p" 
+USE_CACHE = False #set to true to load the pre_computed token
+MAX_WORD = 20000
+MIN_WORD = 5000
 ###
 
 def get_all_file_names(data_path = DATA_PATH):
@@ -32,16 +35,31 @@ def get_all_file_names(data_path = DATA_PATH):
     store_data(file_names,PRE_FILENAME_DATA_PATH)
     return file_names
 
-def get_token_from_file(file_name):
+def is_html(content):
+    html_tag = "<!DOCTYPE html"
+    return content[:len(html_tag)] == html_tag
+
+
+def __get_token_from_file(file_name):
     """
     get all the token from one file, return as a set
     """
     with open(file_name) as f:
-        data = json.load(f)
-        content = data["content"]
-        tokens = nltk.tokenize.word_tokenize(content)
-        filtered_tokens = [word.lower() for word in tokens]
-        return set(filtered_tokens)
+        try:
+            data = json.load(f)
+            content = data["content"] #could be string or html string
+            #check for html file
+            if not is_html(content):
+                return set()
+            soup = BeautifulSoup(content)
+            content = soup.get_text()
+            if len(content) < MIN_WORD or len(content) > MAX_WORD:
+                return set()
+            tokens = nltk.tokenize.word_tokenize(content)
+            filtered_tokens = [word.lower() for word in tokens]
+            return set(filtered_tokens)
+        except:
+            return set()
 
 def get_all_tokens(dataset):
     """
@@ -54,11 +72,15 @@ def get_all_tokens(dataset):
     print("generating tokens...")
     tokens = set()
     size = len(dataset)
-    print_every = 500 #print every 500 files
+    print_every = 1000 #print every 500 files
+    begin = time.time()
+    end = time.time()
     for i,filename in enumerate(dataset):
-        tokens = tokens.union(get_token_from_file(filename))
+        tokens = tokens.union(__get_token_from_file(filename))
         if (i+1)%print_every == 0:
-            print(f"{i+1} files completed, {100*(i+1)/size:.2f}%")
+            end = time.time()
+            print(f"{i+1} files completed, {100*(i+1)/size:.2f}%  time {(end-begin):.3f}s")
+            begin = end
     print(f"token generated completed with size {len(tokens)}")
     tokens = list(tokens)
     store_data(tokens,PRE_TOKEN_DATA_PATH)
@@ -76,7 +98,6 @@ def read_data(filename):
 def main():
     dataset = get_all_file_names(DATA_PATH)
     tokens = get_all_tokens(dataset)
-    
     
     
 
