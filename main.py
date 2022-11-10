adb7dcf68071a5de527fcb605bba2adf09c494bd
Eric <xyy1415238157@icloud.com>
2022-11-09 21:54:54 -0800

    added normalizing and stemming, able to reduct number of tokens from 66134 to 56968

--- main.py
+++ main.py
@@ -5,17 +5,58 @@ from nltk.stem import PorterStemmer
 import pickle
 import time
 from bs4 import BeautifulSoup
+import re
+import string
 
 ### Global variables, 
 DATA_PATH = "./data/DEV"
 PRE_TOKEN_DATA_PATH = "./data/tokens_DEV_cache.p"
 PRE_FILENAME_DATA_PATH = "./data/filenames_DEV_cache.p" 
-USE_CACHE = False #set to true to load the pre_computed token
+USE_CACHE = True #set to true to load the pre_computed token
 MAX_WORD = 20000
 MIN_WORD = 5000
 PS = PorterStemmer()
 ###
 
+
+
+##### Private/Helper/Inner function ---- usually called by other important function
+def is_html(content):
+    html_tag = "<!DOCTYPE html"
+    return content[:len(html_tag)] == html_tag
+
+def get_token_from_file(file_name):
+    """
+    get all the token from one file, return as a set
+    """
+    with open(file_name) as f:
+        try:
+            data = json.load(f)
+            content = data["content"] #could be string or html string
+            #check for html file
+            if not is_html(content):
+                return set()
+            soup = BeautifulSoup(content)
+            content = soup.get_text()
+            if len(content) < MIN_WORD or len(content) > MAX_WORD:
+                return set()
+            tokens = nltk.tokenize.word_tokenize(content)
+            filtered_tokens = [word.lower() for word in tokens]
+            return set(filtered_tokens)
+        except:
+            return set()
+
+def store_data(data,filename=PRE_TOKEN_DATA_PATH):
+    with open(filename,"wb") as f:
+        pickle.dump(data,f)
+    print(f"writting data to {filename}")
+
+def read_data(filename):
+    with open(filename,"rb") as f:
+        return pickle.load(f)
+#####
+
+##### Public function, ----important one, user can call these
 def get_all_file_names(data_path=DATA_PATH):
     """
     get all the file names from a data_path directory
@@ -37,31 +78,6 @@ def get_all_file_names(data_path=DATA_PATH):
     store_data(file_names, PRE_FILENAME_DATA_PATH)
     return file_names
 
-def is_html(content):
-    html_tag = "<!DOCTYPE html"
-    return content[:len(html_tag)] == html_tag
-
-
-def __get_token_from_file(file_name):
-    """
-    get all the token from one file, return as a set
-    """
-    with open(file_name) as f:
-        try:
-            data = json.load(f)
-            content = data["content"] #could be string or html string
-            #check for html file
-            if not is_html(content):
-                return set()
-            soup = BeautifulSoup(content)
-            content = soup.get_text()
-            if len(content) < MIN_WORD or len(content) > MAX_WORD:
-                return set()
-            tokens = nltk.tokenize.word_tokenize(content)
-            filtered_tokens = [word.lower() for word in tokens]
-            return set(filtered_tokens)
-        except:
-            return set()
 
 def get_all_tokens(dataset):
     """
@@ -78,7 +94,7 @@ def get_all_tokens(dataset):
     begin = time.time()
     end = time.time()
     for i,filename in enumerate(dataset):
-        tokens = tokens.union(__get_token_from_file(filename))
+        tokens = tokens.union(get_token_from_file(filename))
         if (i+1)%print_every == 0:
             end = time.time()
             print(f"{i+1} files completed, {100*(i+1)/size:.2f}%  time {(end-begin):.3f}s")
@@ -88,18 +104,30 @@ def get_all_tokens(dataset):
     store_data(tokens,PRE_TOKEN_DATA_PATH)
     return tokens
 
-def store_data(data,filename=PRE_TOKEN_DATA_PATH):
-    with open(filename,"wb") as f:
-        pickle.dump(data,f)
-    print(f"writting data to {filename}")
 
-def read_data(filename):
-    with open(filename,"rb") as f:
-        return pickle.load(f)
+def normalize(tokenlist):
+    #remove punctuation
+    filter_tokens = [re.sub(r'[^\w\s]', '', token) for token in tokenlist]
+    #lower
+    filter_tokens = [token.lower() for token in tokenlist]
+    #stemming
+    filter_tokens = [PS.stem(token,to_lowercase=True) for token in tokenlist]
+    #removing duplicate
+    unique = set()  
+    result = []
+    for token in filter_tokens:
+        if token not in unique:
+            result.append(token)
+            unique.add(token)
+    return result
+
 
 def main():
     dataset = get_all_file_names(DATA_PATH)
     tokens = get_all_tokens(dataset)
+    print(f"read {len(tokens)} tokens from cache")
+    tokens = normalize(tokens)
+    print("after normalzing:",len(tokens),tokens[:20])
     
     
 
