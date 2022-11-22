6acf3d9afff0d2e8bf1208b26b730f9961acf7e7
Eric <xyy1415238157@icloud.com>
2022-11-21 18:31:20 -0800

    optimize lookup table
    open index table once, and keep it open until the search end, minimize query time by not opening the file for every search term

--- search.py
+++ search.py
@@ -10,14 +10,17 @@ import math
 
 
 # this function uses token_pos to find each token in index_table and return is as str
-def find_posting_using_token_pos(postion_lookup_table,token):
+def find_posting_using_token_pos(postion_lookup_table,token,file):
     if token not in postion_lookup_table:
         return []
     position = postion_lookup_table[token]
-    with open("./data/index_table.txt", "rb") as f:
-        f.seek(position)
-        curr_token,posting = f.readline().decode("utf-8").strip().split(":")
-        return eval(posting)
+    file.seek(position)
+    curr_token,posting = file.readline().decode("utf-8").strip().split(":")
+    return eval(posting)
+    # with open("./data/index_table.txt", "rb") as f:
+    #     f.seek(position)
+    #     curr_token,posting = f.readline().decode("utf-8").strip().split(":")
+    #     return eval(posting)
 
 def get_token_pos_eric(index_table_name,destination_filename):
     begin = time.time()
@@ -72,29 +75,29 @@ def main():
         get_token_pos_eric("./data/index_table.txt",postion_lookup_table_file_name)   # if using get_token_pos() to get token positions
     postion_lookup_table = util.read_data(postion_lookup_table_file_name)
     all_indexed_url = util.read_data(util.PRE_INDEXED_URL_PATH)
-
-    #get_file_counts()  # get number of tokens in each file, stores them in file_counts.p
-    while True:
-        # get query tokens
-        print("Enter query ('quit' to end): ")
-        query = str(input())
-        if query == "quit":
-            break
-        ##query_tokens = tokenize.word_tokenize(query) [ERIC] since we used regex to tokenize the sentence, we should use regex here as well
-        begin = time.time()
-        regex_expression = r"[a-zA-Z\d]+"
-        query_tokens = re.findall(regex_expression,query)
-        query_tokens = util.normalize(query_tokens)
-        query_dict = {}
-        posting_begin = time.time()
-        for token in query_tokens:
-            query_dict[token] = find_posting_using_token_pos(postion_lookup_table,token)
-        posing_end = time.time()
-        print(f"Finding Post time: {posing_end-posting_begin:.3f}")
-        intersect = get_intersect_posting(query_dict)
-        top5_urls = [all_indexed_url[docID] for docID, _ in intersect]
-        end = time.time()
-        print(f"Top {len(top5_urls)} results: {top5_urls} \nQuery time {end-begin:.3f}")
+    with open("./data/index_table.txt", "rb") as file:
+        #get_file_counts()  # get number of tokens in each file, stores them in file_counts.p
+        while True:
+            # get query tokens
+            print("Enter query ('quit' to end): ")
+            query = str(input())
+            if query == "quit":
+                break
+            ##query_tokens = tokenize.word_tokenize(query) [ERIC] since we used regex to tokenize the sentence, we should use regex here as well
+            begin = time.time()
+            regex_expression = r"[a-zA-Z\d]+"
+            query_tokens = re.findall(regex_expression,query)
+            query_tokens = util.normalize(query_tokens)
+            query_dict = {}
+            posting_begin = time.time()
+            for token in query_tokens:
+                query_dict[token] = find_posting_using_token_pos(postion_lookup_table,token,file)
+            posing_end = time.time()
+            print(f"Finding Post time: {posing_end-posting_begin:.3f}")
+            intersect = get_intersect_posting(query_dict)
+            top5_urls = [all_indexed_url[docID] for docID, _ in intersect]
+            end = time.time()
+            print(f"Top {len(top5_urls)} results: {top5_urls} \nQuery time {end-begin:.3f}")
 
 
 def get_intersect_posting(query_dict):
